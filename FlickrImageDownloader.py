69717b8d5c9dc356ddd41a937d68f3a5fb9d28a2
Ryan D'souza <dsouzarc@gmail.com>
2015-02-04 13:31:34 -0500

    First commit

---
+++ FlickrImageDownloader.py
@@ -0,0 +1,4 @@
+#Written by Ryan D'souza
+#Receives the URL to a beautiful picture hosted on Flickr that cannot be downloaded
+#Finds the API link that holds the picture
+#Downloads the picture to the directory based on that link
