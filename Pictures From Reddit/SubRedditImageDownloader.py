747697e17e7a955de21ef110620d7e3ffa3702a6
Ryan D'souza <dsouzarc@gmail.com>
2015-07-18 19:04:31 -0400

    More OO design + main method + creates RedditPost objects and adds them to global array

--- Pictures From Reddit/SubRedditImageDownloader.py
+++ Pictures From Reddit/SubRedditImageDownloader.py
@@ -4,32 +4,55 @@ import sys;
 import urllib;
 import urllib2;
 
-tempURL = "http://www.reddit.com/r/winterporn/top.json?sort=top&t=all";
+class SubRedditImageDownloader:
 
-response = urllib2.urlopen(tempURL);
-response = json.load(response);
+        generatedURL = None;
+        redditPosts = None;
 
-jsonPosts = response["data"]["children"];
-redditPosts = {};
+        maxNumberOfPhotos = None;
+        minUpvotes = None;
+        minPhotoWidth = None;
+        minPhotoHeight = None;
 
-for jsonPost in jsonPosts:
-    jsonPost = jsonPost["data"];
-    domain = jsonPost["domain"];
-    score = jsonPost["score"];
-    title = jsonPost["title"];
-    url = jsonPost["url"];
+        def __init__(self, subreddit, maxNumberOfPhotos=10000, minUpvotes=0, minPhotoWidth=1280, minPhotoHeight=800):
+            self.generatedURL = "http://www.reddit.com/r/" + subreddit + "/top.json?sort=top&t=all";
+            self.redditPosts = [];
 
-    print(title);
+            self.maxNumberOfPhotos = maxNumberOfPhotos;
+            self.minUpvotes = minUpvotes;
+            self.minPhotoWidth = minPhotoWidth;
+            self.minPhotoHeight = minPhotoHeight;
 
+            self.getRedditPosts(self.generatedURL);
 
-    class RedditPost(object):
-        domain = None;
-        score = None;
-        title = None;
-        url = None;
+        def getRedditPosts(self, redditURL):
+            response = urllib2.urlopen(redditURL);
+            response = json.load(response);
 
-        def __init__(self, domain, score, title, url):
-            self.domain = domain;
-            self.score = score;
-            self.title = title;
-            self.url = url;
+            jsonPosts = response["data"]["children"];
+
+            for jsonPost in jsonPosts:
+                jsonPost = jsonPost["data"];
+                domain = jsonPost["domain"];
+                score = jsonPost["score"];
+                title = jsonPost["title"];
+                url = jsonPost["url"];
+
+                redditPost = RedditPost(domain, score, title, url);
+                self.redditPosts.append(redditPost);
+
+                
+class RedditPost:
+    domain = None;
+    score = None;
+    title = None;
+    url = None;
+
+    def __init__(self, domain, score, title, url):
+        self.domain = domain;
+        self.score = score;
+        self.title = title;
+        self.url = url;
+
+if __name__ == "__main__":
+    downloader = SubRedditImageDownloader("winterporn");
